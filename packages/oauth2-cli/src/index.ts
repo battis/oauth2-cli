f000b56a587c021d64a294ff33d42fa3966afd38
Seth Battis <seth@battis.net>
2025-03-08 15:26:25 -0500

    feat(oauth2-cli): export Credentials type for convenience

--- packages/oauth2-cli/src/index.ts
+++ packages/oauth2-cli/src/index.ts
@@ -1,6 +1,6 @@
 import { Client } from './Client.js';
 
-export { Client } from './Client.js';
+export { Client, Options as Credentials } from './Client.js';
 export { FileStorage } from './FileStorage.js';
 export { Token } from './Token.js';
 export { TokenStorage } from './TokenStorage.js';
