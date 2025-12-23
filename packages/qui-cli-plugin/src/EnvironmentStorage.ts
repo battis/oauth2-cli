35596808f38e65e4a1ce16e2273bc894b1b0b1ea
Seth Battis <seth@battis.net>
2025-12-23 11:55:21 -0500

    refactor: detect missing token data

--- packages/qui-cli-plugin/src/EnvironmentStorage.ts
+++ packages/qui-cli-plugin/src/EnvironmentStorage.ts
@@ -1,14 +1,19 @@
 import { Env } from '@qui-cli/env-1password';
 import { Token, TokenStorage } from 'oauth2-cli';
+import { TokenEndpointResponse } from 'openid-client';
 
 export class EnvironmentStorage implements TokenStorage {
   public constructor(private tokenEnvVar = 'ACCESS_TOKEN') {}
 
   public async load(): Promise<Token | undefined> {
     try {
-      return Token.fromResponse(
-        JSON.parse(await Env.get({ key: this.tokenEnvVar }))
-      );
+      const data = JSON.parse(
+        await Env.get({ key: this.tokenEnvVar })
+      ) as TokenEndpointResponse;
+      if (!data.access_token) {
+        throw new Error('No access token');
+      }
+      return Token.fromResponse(data);
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (_) {
       return undefined;
