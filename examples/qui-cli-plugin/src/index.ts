9bbc5485697c73b5e4b12a723f1e6ed055810d30
Seth Battis <seth@battis.net>
2025-12-23 10:10:46 -0500

    refactor: update to use current usage

--- examples/qui-cli-plugin/src/index.ts
+++ examples/qui-cli-plugin/src/index.ts
@@ -2,13 +2,10 @@ import { OAuth2 } from '@oauth2-cli/qui-cli-plugin';
 import CLI from '@qui-cli/qui-cli';
 import path from 'node:path';
 
-await CLI.configure({
-  root: { root: path.dirname(import.meta.dirname) },
-  env: { path: path.join(import.meta.dirname, '../.env') }
-});
-await CLI.init();
-console.log(
-  await OAuth2.requestJSON(
+CLI.root.configure({ root: path.dirname(import.meta.dirname) });
+await CLI.run();
+CLI.log.info({
+  self: await OAuth2.requestJSON(
     new URL('/api/v1/users/self', process.env.AUTHORIZATION_ENDPOINT)
   )
-);
+});
