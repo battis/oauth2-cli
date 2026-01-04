3fe454f28497d704041ea4e599a4ad5b2b08b469
Seth Battis <seth@battis.net>
2026-01-04 15:55:20 -0500

    feat: provide fetch() and fetchJSON() methods to translate to openid-client requests

--- packages/oauth2-cli/src/Client.ts
+++ packages/oauth2-cli/src/Client.ts
@@ -1,4 +1,5 @@
 import * as Configuration from '@battis/oauth2-configure';
+import { JSONValue } from '@battis/typescript-tricks';
 import { Mutex } from 'async-mutex';
 import * as OpenIDClient from 'openid-client';
 import { FileStorage } from './FileStorage.js';
@@ -6,30 +7,32 @@ import * as Localhost from './Localhost.js';
 import { Token } from './Token.js';
 import { TokenStorage } from './TokenStorage.js';
 
-export type Options = Configuration.Options & {
+export type Credentials = Configuration.Options & {
   scope?: string;
   headers?: Record<string, string>;
   parameters?: Record<string, string>;
   store?: TokenStorage | string;
 };
 
+/** Wrap an OpenID configuration in an object-oriented API client. */
 export class Client {
   private tokenMutex = new Mutex();
   private config?: OpenIDClient.Configuration;
   private token?: Token;
   private store?: TokenStorage;
 
-  public constructor(private options: Options) {
-    if (this.options.store) {
-      if (typeof this.options.store === 'string') {
-        this.store = new FileStorage(this.options.store);
+  public constructor(private credentials: Credentials) {
+    if (this.credentials.store) {
+      if (typeof this.credentials.store === 'string') {
+        this.store = new FileStorage(this.credentials.store);
       } else {
-        this.store = this.options.store;
+        this.store = this.credentials.store;
       }
     }
   }
 
-  public async getToken() {
+  /** Acquire a valid, unexpired API access token. */
+  public async getToken(): Promise<Token | undefined> {
     return await this.tokenMutex.runExclusive(
       (async () => {
         if (!this.token) {
@@ -46,14 +49,15 @@ export class Client {
     );
   }
 
+  /** Refresh Token Grant */
   private async refreshToken(token: Token): Promise<Token | undefined> {
     if (token.refresh_token) {
-      const { headers, parameters } = this.options;
+      const { headers, parameters } = this.credentials;
       let freshTokens;
       if (
         (freshTokens = Token.fromResponse(
           await OpenIDClient.refreshTokenGrant(
-            await Configuration.acquire(this.options),
+            await Configuration.acquire(this.credentials),
             token.refresh_token,
             parameters,
             // @ts-expect-error 2322 undocumented arg pass-through to oauth4webapi
@@ -71,19 +75,21 @@ export class Client {
     return this.authorize();
   }
 
+  /** Acquire a valid OpenID configuration. */
   private async getConfiguration() {
     if (!this.config) {
-      this.config = await Configuration.acquire(this.options);
+      this.config = await Configuration.acquire(this.credentials);
     }
     return this.config;
   }
 
+  /** Authorization Code Grant */
   private async authorize(): Promise<Token | undefined> {
     const {
       scope,
       redirect_uri,
       parameters: additionalParameters
-    } = this.options;
+    } = this.credentials;
 
     return new Promise((resolve, reject) => {
       const code_verifier = OpenIDClient.randomPKCECodeVerifier();
@@ -108,7 +114,7 @@ export class Client {
           }
 
           await Localhost.redirectServer({
-            ...this.options,
+            ...this.credentials,
             authorization_url: OpenIDClient.buildAuthorizationUrl(
               await this.getConfiguration(),
               parameters
@@ -129,33 +135,80 @@ export class Client {
     });
   }
 
+  /**
+   * Make an authorized request to the API, acquiring an unexpired acess token
+   * if necessary.
+   */
   public async request(
     url: URL | string,
     method: string = 'GET',
     body?: OpenIDClient.FetchBody,
-    headers?: Record<string, string>,
-    options?: OpenIDClient.DPoPOptions
+    headers?: Headers,
+    dPoPOptions?: OpenIDClient.DPoPOptions
   ) {
+    for (const header in this.credentials.headers) {
+      headers?.append(header, this.credentials.headers[header]);
+    }
     return await OpenIDClient.fetchProtectedResource(
       await this.getConfiguration(),
       (await this.getToken())!.access_token,
       new URL(url),
       method,
       body,
-      new Headers({ ...this.options.headers, ...headers }),
-      options
+      headers,
+      dPoPOptions
     );
   }
 
-  public async requestJSON(
+  /**
+   * Make an authorized request to the API, acquiring an unexpired acess token
+   * if necessary.
+   *
+   * This converts the Fetch API request into an OpenID request. To directly
+   * make this request, use {@link request()}
+   */
+  public async fetch(
+    endpoint: string | URL | Request,
+    init?: RequestInit
+  ): Promise<Response> {
+    return this.request(
+      endpoint.toString(),
+      init?.method,
+      init?.body?.toString(),
+      new Headers(init?.headers)
+    );
+  }
+
+  /**
+   * Make an authorized request to the API, acquiring an unexpired acess token
+   * if necessary and parse the JSON respomse.
+   *
+   * @param validator Optional validator function test that the JSON response is
+   *   the expected type.
+   */
+  public async requestJSON<T extends JSONValue = JSONValue>(
     url: URL | string,
     method: string = 'GET',
     body?: OpenIDClient.FetchBody,
-    headers?: Record<string, string>,
-    options?: OpenIDClient.DPoPOptions
+    headers?: Headers,
+    dPoPOptions?: OpenIDClient.DPoPOptions
   ) {
-    return await (
-      await this.request(url, method, body, headers, options)
-    ).json();
+    return (await (
+      await this.request(url, method, body, headers, dPoPOptions)
+    ).json()) as T;
+  }
+
+  /**
+   * Make an authorized request to the API, acquiring an unexpired acess token
+   * if necessary and parse the JSON respomse.
+   *
+   * This converts the Fetch API request into an OpenID request. To directly
+   * make this request, use {@link requestJSON()}
+   */
+  public async fetchJSON<T extends JSONValue = JSONValue>(
+    endpoint: string | URL | Request,
+    init?: RequestInit
+  ) {
+    return (await (await this.fetch(endpoint, init)).json()) as T;
   }
 }
