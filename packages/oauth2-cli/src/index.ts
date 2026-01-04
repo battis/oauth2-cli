2dde67edd70b151bd1bab6de3845839a06957e65
Seth Battis <seth@battis.net>
2026-01-04 15:54:20 -0500

    fix!: remove deprecated TokenManager
    BREAKING: TokenManager has been removed, use Client

--- packages/oauth2-cli/src/index.ts
+++ packages/oauth2-cli/src/index.ts
@@ -1,11 +1,4 @@
-import { Client } from './Client.js';
-
-export { Client, Options as Credentials } from './Client.js';
-export { FileStorage } from './FileStorage.js';
-export { Token } from './Token.js';
-export { TokenStorage } from './TokenStorage.js';
-
-export {
-  /** @deprecated use Client */
-  Client as TokenManager
-};
+export * from './Client.js';
+export * from './FileStorage.js';
+export * from './Token.js';
+export * from './TokenStorage.js';
