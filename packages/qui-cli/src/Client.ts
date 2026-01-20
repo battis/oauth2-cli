fda83da8794d2f7b7221224619b34148ae0a8fe1
Seth Battis <seth@battis.net>
2026-01-20 12:58:54 -0500

    fix: further token managment debugging output

--- packages/qui-cli/src/Client.ts
+++ packages/qui-cli/src/Client.ts
@@ -1,9 +1,29 @@
 import { JSONValue } from '@battis/typescript-tricks';
 import { Log } from '@qui-cli/log';
 import * as OAuth2CLI from 'oauth2-cli';
-import { DPoPOptions, FetchBody } from 'openid-client';
+import { Configuration, DPoPOptions, FetchBody } from 'openid-client';
 
 export class Client extends OAuth2CLI.Client {
+  protected async getConfiguration(): Promise<Configuration> {
+    const config = await super.getConfiguration();
+    Log.debug('OAuth 2.0 configuration', config);
+    return config;
+  }
+
+  protected async authorize(): Promise<OAuth2CLI.Token | undefined> {
+    Log.debug('Authorizing new access token');
+    return await super.authorize();
+  }
+
+  protected async refreshToken(
+    token: OAuth2CLI.Token
+  ): Promise<OAuth2CLI.Token | undefined> {
+    Log.debug('Refreshing expired access token', { token });
+    const refreshed = await super.refreshToken(token);
+    Log.debug('Received refreshed access token', { token: refreshed });
+    return refreshed;
+  }
+
   public async request(
     url: URL | string,
     method?: string,
