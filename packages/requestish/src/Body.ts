13f80c986a01e88f1f45c48873fdc10a58852a4b
Seth Battis <seth@battis.net>
2026-03-05 13:19:54 -0500

    fix: also allow Body to use URLSearchParams to accept JSON primitive values

--- packages/requestish/src/Body.ts
+++ packages/requestish/src/Body.ts
@@ -1,14 +1,23 @@
-import { isRecord } from '@battis/typescript-tricks';
+import { isRecord, JSONPrimitive } from '@battis/typescript-tricks';
 import type { FetchBody } from 'openid-client';
+import { from as URLSearchParamsFrom } from './URLSearchParams.js';
 
 export type ish =
   | FetchBody
   | FormData
-  | Record<string, string>
+  | Record<string, JSONPrimitive>
   | RequestInit['body'];
 
 function isString(obj: unknown): obj is string {
   return typeof obj === 'string';
+}
+
+function isJSONPrimitive(obj: unknown): obj is JSONPrimitive {
+  return (
+    typeof obj === 'string' ||
+    typeof obj === 'number' ||
+    typeof obj === 'boolean'
+  );
 }
 
 export async function from(body: ish): Promise<FetchBody | undefined> {
@@ -22,8 +31,8 @@ export async function from(body: ish): Promise<FetchBody | undefined> {
     body instanceof URLSearchParams
   ) {
     return body;
-  } else if (isRecord<string, string>(body, isString, isString)) {
-    return new URLSearchParams(body);
+  } else if (isRecord<string, JSONPrimitive>(body, isString, isJSONPrimitive)) {
+    return URLSearchParamsFrom(body);
   }
   return new Response(body).arrayBuffer();
 }
