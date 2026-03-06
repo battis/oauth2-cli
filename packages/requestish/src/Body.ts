bd0f78c0906b9a09be8f450d488533036114add4
Seth Battis <seth@battis.net>
2026-03-06 08:10:19 -0500

    refactor: export isRecord tests to their own file for reuse

--- packages/requestish/src/Body.ts
+++ packages/requestish/src/Body.ts
@@ -1,24 +1,13 @@
 import { isRecord, JSONPrimitive } from '@battis/typescript-tricks';
 import type { FetchBody } from 'openid-client';
 import { from as URLSearchParamsFrom } from './URLSearchParams.js';
+import { isJSONPrimitive, isString } from './isRecord.js';
 
 export type ish =
   | FetchBody
   | FormData
   | Record<string, JSONPrimitive>
   | RequestInit['body'];
-
-function isString(obj: unknown): obj is string {
-  return typeof obj === 'string';
-}
-
-function isJSONPrimitive(obj: unknown): obj is JSONPrimitive {
-  return (
-    typeof obj === 'string' ||
-    typeof obj === 'number' ||
-    typeof obj === 'boolean'
-  );
-}
 
 export async function from(body: ish): Promise<FetchBody | undefined> {
   if (
---
+++ packages/requestish/src/isRecord.ts
@@ -0,0 +1,13 @@
+import { JSONPrimitive } from '@battis/typescript-tricks';
+
+export function isString(obj: unknown): obj is string {
+  return typeof obj === 'string';
+}
+
+export function isJSONPrimitive(obj: unknown): obj is JSONPrimitive {
+  return (
+    typeof obj === 'string' ||
+    typeof obj === 'number' ||
+    typeof obj === 'boolean'
+  );
+}
