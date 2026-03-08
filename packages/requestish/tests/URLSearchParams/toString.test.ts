ee74cf5189bd519a5e913620ecfbdfabf8bb109e
Seth Battis <seth@battis.net>
2026-03-08 12:21:24 -0400

    test: more complete toString() tests

--- packages/requestish/tests/URLSearchParams/toString.test.ts
+++ packages/requestish/tests/URLSearchParams/toString.test.ts
@@ -20,3 +20,20 @@ test('multiples', () => {
     ])
   ).toEqual('?a=A&a=B');
 });
+
+test('ish', () => {
+  expect(toString({ a: 1, b: 2 })).toEqual('?a=1&b=2');
+  expect(toString({ a: 1, b: undefined, c: 3 })).toEqual('?a=1&c=3');
+  expect(
+    toString([
+      ['a', 'A'],
+      ['a', 'B']
+    ])
+  ).toEqual('?a=A&a=B');
+  expect(
+    toString([
+      ['a', 1],
+      ['b', undefined]
+    ])
+  ).toEqual('?a=1&b=');
+});
