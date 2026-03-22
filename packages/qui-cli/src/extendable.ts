a6854c87efbdf004807ace4587cfd828f9b52e23
Seth Battis <seth@battis.net>
2026-03-21 21:55:15 -0400

    fix: export PaginatedCollection

--- packages/qui-cli/src/extendable.ts
+++ packages/qui-cli/src/extendable.ts
@@ -1,4 +1,10 @@
-export { Credentials, Injection, Localhost, Options } from 'oauth2-cli';
+export {
+  Credentials,
+  Injection,
+  Localhost,
+  Options,
+  PaginatedCollection
+} from 'oauth2-cli';
 export * from './Client.js';
 export * from './OAuth2Plugin.js';
 export * as Token from './Token/index.js';
